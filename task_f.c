2fc5f5b049cbcac25d2a07393d07860df2940248
Bo Zhang <bo.zhang-17@student.manchester.ac.uk>
2024-05-05 11:09:03 +0100

    v2

--- task_f.c
+++ task_f.c
@@ -1,27 +1,36 @@
+// Includes
 #include <stdio.h>
 #include <string.h>
 #include "domains.h"
 
+// Main function 
 int main(void) {
     
+    // Read user input
     #define LEN 4
     char user_input[LEN];
     printf("Enter a domain including the . (e.g. .uk): ");
     fgets(user_input, LEN, stdin);
     
+	
+    // Enter your code under here
+    // Use strcmp(user_input, country_codes[0].domain) to compare the text in user_input to the text in country_codes[0].domain.
     int i = 0;
     int x = 0;
     
+    // Loop through the array of ccTLDs to find a match
     do {
-        x = strcmp(user_input, country_codes[i].domain);
         i++;
-    } while (x != 0 && i < 258);
+        x = strcmp(user_input, country_codes[i].domain);
+    } while (x != 0 && i != 258);
 
+    // Display the corresponding country if a match is found, otherwise display "Domain not found"
     if (x == 0) {
-        printf("This is the domain for: %s", country_codes[i - 1].country);
+        printf("This is the domain for: %s", country_codes[i].country);
     } else {
-        printf("Domain not found.");
+        printf("Domain not found");
     }
 
+    // Do not edit below here
     return 0;
 }
