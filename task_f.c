b1a335fbb1fe5c37ec367da045efc1a0a9390b03
Bo Zhang <bo.zhang-17@student.manchester.ac.uk>
2024-05-05 11:24:43 +0100

    v8

--- task_f.c
+++ task_f.c
@@ -1,15 +1,19 @@
+// Includes
 #include <stdio.h>
 #include <string.h>
 #include "domains.h"
 
+// Main function 
 int main(void) {
+
     // Read user input
     #define LEN 4
     char user_input[LEN];
     printf("Enter a domain including the . (e.g. .uk): ");
     fgets(user_input, LEN, stdin);
     
-    // Check if the first character entered is a dot
+    // Enter your code under here
+    // Use strcmp(user_input, country_codes[0].domain) to compare the text in user_input to the text in country_codes[0].domain.
     if (user_input[0] != '.') {
         printf("Error! The first character entered should be a .");
         return 0;
@@ -28,6 +32,7 @@ int main(void) {
     } else {
         printf("Domain not found.");
     }
-
+    
+    // Do not edit below here
     return 0;
 }
